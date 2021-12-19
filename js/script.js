965a30353bc301d86dbfaf899a57b817507a96bb
Leandro Jesus Do Nascimento <leandronasx18@gmail.com>
2021-12-18 21:56:29 -0300

    Codificacao da page comunidade

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-let projeto = [
-    {
-        codigo:"",
-        nome:"",
-        descricao:"",
-        lang:"",
-        cor:"",
-        curtidas: 0
-    }
-];
+let corDaBarra = document.getElementById("cor-project");
+corDaBarra.value = "#6BD1FF";
+
+corDaBarra.addEventListener("input", function(){
+    document.querySelector(".editor-borda").style.backgroundColor = this.value;
+});
